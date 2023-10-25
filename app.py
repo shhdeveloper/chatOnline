6fe91cbd5a14434bf895398cc4727f7529c7c111
shhdeveloper <shhdeveloper@gmail.com>
2023-10-25 13:32:03 +1030

    s

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-import json
-import requests
+# import json
+# import requests
 
-# Define your string data and file path
-data = "Your string data"
-file_path = "cyclic_data.json"
+# # Define your string data and file path
+# data = "Your string data"
+# file_path = "cyclic_data.json"
 
-# Save the string cyclically to a JSON file
-try:
-    with open(file_path, 'r') as file:
-        cyclic_data = json.load(file)
-except (FileNotFoundError, json.JSONDecodeError):
-    cyclic_data = []
+# # Save the string cyclically to a JSON file
+# try:
+#     with open(file_path, 'r') as file:
+#         cyclic_data = json.load(file)
+# except (FileNotFoundError, json.JSONDecodeError):
+#     cyclic_data = []
 
-cyclic_data.append(data)
+# cyclic_data.append(data)
 
-with open(file_path, 'w') as file:
-    json.dump(cyclic_data, file)
+# with open(file_path, 'w') as file:
+#     json.dump(cyclic_data, file)
 
-# Load the cyclic data from the file
-with open(file_path, 'r') as file:
-    cyclic_data = json.load(file)
+# # Load the cyclic data from the file
+# with open(file_path, 'r') as file:
+#     cyclic_data = json.load(file)
 
-# Define the URL of your web service
-url = "https://gorgeous-lime-seagull.cyclic.app/api/data"  # Replace with your actual URL
+# # Define the URL of your web service
+# url = "https://gorgeous-lime-seagull.cyclic.app/api/data"  # Replace with your actual URL
 
-# Send a POST request with JSON data
-data_to_send = {
-    "cyclic_data": cyclic_data
-}
+# # Send a POST request with JSON data
+# data_to_send = {
+#     "cyclic_data": cyclic_data
+# }
 
-response = requests.post(url, json=data_to_send)
+# response = requests.post(url, json=data_to_send)
 
-# Check the response
-if response.status_code == 200:
-    print("Data sent successfully")
+# # Check the response
+# if response.status_code == 200:
+#     print("Data sent successfully")
 
-    # Retrieve the list of saved data from the response
-    response_data = response.json()
-    saved_data = response_data.get("saved_data", [])
-    print("List of saved data:", saved_data)
-else:
-    print(f"Error: {response.status_code}")
+#     # Retrieve the list of saved data from the response
+#     response_data = response.json()
+#     saved_data = response_data.get("saved_data", [])
+#     print("List of saved data:", saved_data)
+# else:
+#     print(f"Error: {response.status_code}")
 
 
 
